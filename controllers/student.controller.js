77ad1f6817af922fc0c21ab44df76a6f976f7d9c
02bamba <cabkebe02@gmail.com>
2024-07-20 17:34:14 +0200

    first commit

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -46,3 +46,96 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+// Find a single Student with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Student.findOne(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Student with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Student with id=" + id });
+    });
+};
+
+// Find all registered Students
+exports.findAllRegistered = (req, res) => {
+  Student.find({ registered: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving students.",
+      });
+    });
+};
+
+// Update a Student by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+  const id = req.params.id;
+  Student.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Student with id=${id}. Maybe Student was not found!`,
+        });
+      } else res.send({ message: "Student was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Student with id=" + id,
+      });
+    });
+};
+
+
+
+// Delete a Student with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Student.findByIdAndDelete(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Student with id=${id}. Maybe Student was not found!`,
+        });
+      } else {
+        res.send({
+          message: "Student was deleted successfully!",
+        });
+      }
+    })
+.catch(err => {
+      res.status(500).send({
+        message: "Could not delete Student with id=" + id,
+      });
+    });
+};
+
+// Delete all Students from the database.
+exports.deleteAll = (req, res) => {
+  Student.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Students were deleted successfully!`,
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all students.",
+      });
+    });
+};
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -8,5 +8,22 @@ module.exports = app => {
   // Retrieve all Students
   router.get("/", students.findAll);
 
+  // Retrieve all registered Students
+  router.get("/registered", students.findAllRegistered);
+
+
+  // Retrieve a single Student with id
+  router.get("/:id", students.findOne);
+
+
+  // Update a Student with id
+  router.put("/:id", students.update);
+
+  // Delete a Student with id
+  router.delete("/:id", students.delete);
+// Delete all students
+  router.delete("/", students.deleteAll);
+
   app.use("/api/students", router);
+
 };
